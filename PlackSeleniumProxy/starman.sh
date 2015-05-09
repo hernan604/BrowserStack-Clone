770588ce2a9dd4ae6d2eebdbfd7f6000c8952f1c
Hernan Lopes <hernanlopes@gmail.com>
2015-05-09 19:55:29 -0300

    renamed and deleted PlackSeleniumProxy namespaces

--- PlackSeleniumProxy/starman.sh
+++ PlackSeleniumProxy/starman.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ../env.sh
-starman -I../Mojo-BrowserStack-DB/lib/ -I./lib --listen :5555 --workers 32 --preload-app plack.pl
+starman -I../Mojo-BrowserStack-DB/lib/ -I./lib --listen :5555 --workers 32 --preload-app server.pl
